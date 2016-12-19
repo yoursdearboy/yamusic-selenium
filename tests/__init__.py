4eca065f344919b898578a94c891a896f5652e79
Kirill Voronin <yoursdearboy@gmail.com>
2016-12-19 16:33:17 +0300

    Album tests suite to tests suites (in __init__)

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,9 @@
 import unittest
 from .test_artist import suite as test_artist_suite
+from .test_album import suite as test_album_suite
 
 def suite():
   suite = unittest.TestSuite()  
   suite.addTest(test_artist_suite())
+  suite.addTest(test_album_suite())
   return suite
